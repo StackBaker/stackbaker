fa6212622d309141138ec6d106dc80b0addb8cb9
spectraldoy <littleguyisfum@gmail.com>
2023-04-30 11:02:00 -0700

    version 0.0.3: testing if updater works in build without debug

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -6,13 +6,6 @@ import coordinateBackendAndState from "../coordinateBackendAndState";
 import type { coordinateBackendAndStateProps } from "../coordinateBackendAndState";
 import "../styles.css"
 
-import {
-    checkUpdate,
-    installUpdate,
-    onUpdaterEvent,
-} from '@tauri-apps/api/updater'
-import { relaunch } from '@tauri-apps/api/process'
-
 type DashboardProps = coordinateBackendAndStateProps;
 
 const Dashboard = function(props: DashboardProps) {
@@ -20,36 +13,6 @@ const Dashboard = function(props: DashboardProps) {
     const headerHeight = 50;
     
     const coordination = coordinateBackendAndState(props);
-
-    const func = async () => {
-        const unlisten = await onUpdaterEvent(({ error, status }) => {
-            // This will log all updater events, including status updates and errors.
-            console.log('Updater event', error, status);
-        })
-        
-        // you need to call unlisten if your handler goes out of scope, for example if the component is unmounted.
-        unlisten()
-
-        try {
-            const { shouldUpdate, manifest } = await checkUpdate()
-
-            if (shouldUpdate) {
-                // You could show a dialog asking the user if they want to install the update here.
-                console.log(
-                    `Installing update ${manifest?.version}, ${manifest?.date}, ${manifest?.body}`
-                )
-
-                // Install the update. This will also restart the app on Windows!
-                //await installUpdate()
-
-                // On macOS and Linux you will need to restart the app manually.
-                // You could use this step to display another confirmation dialog.
-                //await relaunch()
-            }
-        } catch (error) {
-            console.error("c", error)
-        }
-    }
 
     // have to do this sx thing because AppShell automatically renders too large
     return (
